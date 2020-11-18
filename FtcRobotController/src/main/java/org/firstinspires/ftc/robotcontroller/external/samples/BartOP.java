92ab170f7022e83580206ed0a40f3dc5e791d72e
Ethan <ethanbart03@gmail.com>
2020-11-17 20:30:06 -0700

    First time commiting test code again

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BartOP.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public abstract class BartOP extends OpMode {
+
+    //Initialize
+    public void init(){
+
+    }
+
+    //Start Press
+    public void start() {
+
+    }
+
+    //Loop
+    public void loop() {
+
+    }
+
+    //Stop
+    public void stop() {
+
+    }
+
+}
